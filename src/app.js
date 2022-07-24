5223092393f8ffe3569db4514c87f7b6f54c8920
Med Yacine Khouini <medyacine.khouini@esprit.tn>
2022-07-24 17:33:49 +0100

    update help text

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'These is some helpful text',
+    helpText: 'This website uses data from mapbox.com and weatherstach.com',
     title: 'Help',
     name: 'Khouini Yacine',
   });
